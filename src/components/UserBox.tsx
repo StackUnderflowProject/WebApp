253ec1923a8f47efe9f0a32452967aebde53b892
Tadej <tadej.tersek03@gmail.com>
2024-05-22 12:02:30 +0200

    SCRUM-23 userbox init

--- src/components/UserBox.tsx
+++ src/components/UserBox.tsx
@@ -10,7 +10,7 @@ import { faAddressCard, faGear, faRightFromBracket } from '@fortawesome/free-sol
 
 function UserBox() {
     
-    const { user } = useUserContext();  // setUserContext
+    const { user, logout } = useUserContext();  // setUserContext
     //const { token, setToken, isTokenExpired } = useJWTContext();
     const [userMenuOpened, setUserMenuOpened] = useState(false);
 
@@ -21,13 +21,13 @@ function UserBox() {
 
     return (
         <div id="user-box">
-            <img src={(user && user.image) ? ("http:/localhost:3000/images/profile_pictures/" + user.image) : ("/defaultProfilePicture.png")} alt="profile-picture" id="profile-picture" onClick={userMenuToggle}></img>
+            <img src={(user && user.image) ? ("http://localhost:3000/images/profile_pictures/" + user.image) : ("/defaultProfilePicture.png")} alt="profile-picture" id="profile-picture" onClick={userMenuToggle}></img>
             {(userMenuOpened) ? 
             (<div>
                 <div id="user-profile-menu">
-                    <Link to="/profile"><div className='option'><FontAwesomeIcon icon={faAddressCard} /><p>Profil</p></div></Link>
+                    <Link to={"/profile/" + user?._id}><div className='option'><FontAwesomeIcon icon={faAddressCard} /><p>Profil</p></div></Link>
                     <Link to="/settings"><div className='option'><FontAwesomeIcon icon={faGear} />Nastavitve</div></Link>
-                    <Link to="/logout"><div className='option'><FontAwesomeIcon icon={faRightFromBracket} />Odjava</div></Link>
+                    <div className='option' onClick={logout}><FontAwesomeIcon icon={faRightFromBracket} />Odjava</div>
                 </div>
             </div>)
             :
