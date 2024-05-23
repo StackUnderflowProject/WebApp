46b191fcaa9ec353053f51f2a2ec12f01d5e4284
Matija Pajenk <matija.pajenk@student.um.si>
2024-05-23 17:15:06 +0200

    SCRUM-117 add matches map

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,41 @@
 import { UserProvider } from './userContext'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import FilterMapComponent from './components/FilterMapComponent.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { FootballStandingsGraph } from './components/FootballStandingsGraph.tsx'
-import { FootballTeamList } from './components/FootballTeamList.tsx'
-import { HandballStandingsGraph } from './components/HandballStandingsGraph.tsx'
-import { HandballTeamList } from './components/HandballTeamList.tsx'
-import { TeamsStats } from './components/TeamsStats.tsx'
 
-import Login from './components/Login.tsx';
-import Register from './components/Register.tsx';
-import Profile from './components/Profile.tsx';
-import Schedule from './components/Schedule.tsx';
-import Standings from './components/Standings.tsx';
-import {FootballTeam} from './components/FootballTeam.tsx';
-import {HandballTeam} from './components/HandballTeam.tsx';
+import Login from './components/Login.tsx'
+import Register from './components/Register.tsx'
+import Profile from './components/Profile.tsx'
+import Schedule from './components/Schedule.tsx'
+import Standings from './components/Standings.tsx'
+import { FootballTeam } from './components/FootballTeam.tsx'
+import { HandballTeam } from './components/HandballTeam.tsx'
+import { HomePage } from './components/HomePage.tsx'
 
 const queryClient = new QueryClient()
 
 function App() {
     return (
-        <>
+        <div className="h-dvh w-[80%] text-center ml-auto mr-auto">
             <BrowserRouter>
                 <UserProvider>
-                        <Navbar />
-                        <QueryClientProvider client={queryClient}>
-                            <Routes>
-                                <Route path="/" element={<>
-                                    <FilterMapComponent />
-                                    <TeamsStats />
-                                    <FootballStandingsGraph />
-                                    <HandballStandingsGraph />
-                                    <FootballTeamList />
-                                    <HandballTeamList />
-                                </>} 
-                                />
-                                <Route path="/register" element={<Register />} /> 
-                                <Route path="/login" element={<Login />} />
-                                <Route path="/profile/:userId" element={<Profile />} />
-                                <Route path="/settings" element={<p></p>} />
-                                <Route path="/schedule" element={<Schedule />} />
-                                <Route path="/standings" element={<Standings />} />
-                                <Route path="/footballTeam/:teamId" element={<FootballTeam />} />
-                                <Route path="/handballTeam/:teamId" element={<HandballTeam />} />                            </Routes>
-                        </QueryClientProvider>
+                    <Navbar />
+                    <QueryClientProvider client={queryClient}>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/profile/:userId" element={<Profile />} />
+                            <Route path="/settings" element={<p></p>} />
+                            <Route path="/schedule" element={<Schedule />} />
+                            <Route path="/standings" element={<Standings />} />
+                            <Route path="/footballTeam/:teamId" element={<FootballTeam />} />
+                            <Route path="/handballTeam/:teamId" element={<HandballTeam />} />{' '}
+                        </Routes>
+                    </QueryClientProvider>
                 </UserProvider>
             </BrowserRouter>
-        </>
+        </div>
     )
 }
 
