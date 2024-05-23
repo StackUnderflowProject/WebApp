b8995c685d4a9b320bca4b9f571960c7e960ccf2
Tadej <tadej.tersek03@gmail.com>
2024-05-23 11:22:14 +0200

    SCRUM-32 moved jwt into user structure

--- src/userContext.tsx
+++ src/userContext.tsx
@@ -1,5 +1,7 @@
 import React, { createContext, useState, useContext, ReactNode } from 'react';
 import {jwtDecode} from 'jwt-decode';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type User = {
     _id: string,
@@ -12,14 +14,20 @@ type User = {
 
 type UserContextType = {
     user: User | null;
-    setUserContext: (user: User | null) => void;
+    login: (newUser: User | null) => void;
+    logout: () => void;
+    updateUser: (newUser: User | null) => void;
     isTokenExpired: () => boolean; 
+    resetJWT: () => void;
 }
 
 export const UserContext = createContext<UserContextType>({
     user: null,
-    setUserContext: () => {},
-    isTokenExpired: () => {return true}
+    login: () => {},
+    logout: () => {},
+    updateUser: () => {},
+    isTokenExpired: () => {return true},
+    resetJWT: () => {}
 });
 
 type UserProviderProps = {
@@ -28,10 +36,34 @@ type UserProviderProps = {
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
+  const navigate = useNavigate();
 
-  const setUserContext = (newUser: User | null) => {
+  useEffect(() => {
+    const userStored = localStorage.getItem('user');
+    if (userStored) {
+      try {
+        const parsedUser: User = JSON.parse(userStored);
+        setUser(parsedUser);
+      } catch (e) {
+        console.error("Failed to parse user from localStorage", e);
+      }
+    }
+  }, []);
+
+  const login = (newUser: User | null) => {
+    localStorage.setItem('user', JSON.stringify(newUser));
     setUser(newUser);
   };
+
+  const logout = () => {
+    localStorage.removeItem('user');
+    setUser(null);
+  };
+
+  const updateUser = (newUser: User | null) => {
+    localStorage.setItem('user', JSON.stringify(newUser));
+    setUser(newUser);
+  }
 
   const isTokenExpired = (): boolean => {
     if (user === null || !user.token) return true;
@@ -39,11 +71,16 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     const decoded: { exp: number } = jwtDecode(user.token);
     const currentTime = Date.now() / 1000;
 
-    return decoded.exp < currentTime;
+    return decoded.exp < currentTime + 1;
+  };
+
+  const resetJWT = () => {
+    setUser(null);
+    navigate('/login');
   };
 
   return (
-    <UserContext.Provider value={{ user, setUserContext, isTokenExpired }}>
+    <UserContext.Provider value={{ user, login, logout, updateUser, isTokenExpired, resetJWT }}>
       {children}
     </UserContext.Provider>
   );
