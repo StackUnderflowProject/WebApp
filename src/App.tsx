b8d1de88c208c00d1862b397f9c1415446a2c0e0
Tadej <tadej.tersek03@gmail.com>
2024-05-27 14:58:12 +0200

    SCRUM-33 work with sockets

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { UserProvider } from './userContext'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WebSocketProvider } from './WebsocketContext.tsx';
 
 import Login from './components/Login.tsx'
 import Register from './components/Register.tsx'
@@ -24,23 +25,25 @@ function App() {
         <div className="h-dvh w-[80%] text-center ml-auto mr-auto">
             <BrowserRouter>
                 <UserProvider>
-                    <Navbar />
-                    <QueryClientProvider client={queryClient}>
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/register" element={<Register />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/profile/:userId" element={<Profile />} />
-                            <Route path="/settings" element={<p></p>} />
-                            <Route path="/schedule" element={<Schedule />} />
-                            <Route path="/standings" element={<Standings />} />
-                            <Route path="/footballTeam/:teamId" element={<FootballTeam />} />
-                            <Route path="/handballTeam/:teamId" element={<HandballTeam />} />
-                            <Route path="/createEvent" element={<CreateEvent />} />
-                            <Route path="/events" element={<EventList />} />
-                            <Route path="/graphs" element={<GraphPage />} />
-                        </Routes>
-                    </QueryClientProvider>
+                    <WebSocketProvider>
+                        <Navbar />
+                        <QueryClientProvider client={queryClient}>
+                            <Routes>
+                                <Route path="/" element={<HomePage />} />
+                                <Route path="/register" element={<Register />} />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/profile/:userId" element={<Profile />} />
+                                <Route path="/settings" element={<p></p>} />
+                                <Route path="/schedule" element={<Schedule />} />
+                                <Route path="/standings" element={<Standings />} />
+                                <Route path="/footballTeam/:teamId" element={<FootballTeam />} />
+                                <Route path="/handballTeam/:teamId" element={<HandballTeam />} />
+                                <Route path="/createEvent" element={<CreateEvent />} />
+                                <Route path="/events" element={<EventList />} />
+                                <Route path="/graphs" element={<GraphPage />} />
+                            </Routes>
+                        </QueryClientProvider>
+                    </WebSocketProvider>
                 </UserProvider>
             </BrowserRouter>
         </div>
