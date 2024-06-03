c3509abccd2a82a99757800634f28122bba2a2ee
Matija Pajenk <matija.pajenk@student.um.si>
2024-06-03 11:45:28 +0200

    SCRUM-144 add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.API_URL': JSON.stringify('http://localhost:3000')
-  }
+    plugins: [react()],
+    define: {
+        'import.meta.env.API_URL': JSON.stringify('http://localhost:3000')
+    },
+    server: {
+        host: true
+    }
 })
